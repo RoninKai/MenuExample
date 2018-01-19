5a36fb10e53eff165d8a895be5eea77e1c53b9f0
zhoukai <189898933@qq.com>
2018-01-19 16:29:34 +0800

    为Menu添加自定义layout，支持文字与图片同时显示。

--- app/src/main/java/com/base/example/MainActivity.java
+++ app/src/main/java/com/base/example/MainActivity.java
@@ -2,6 +2,7 @@ package com.base.example;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
@@ -12,7 +13,13 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
-public class MainActivity  extends BaseActivity implements View.OnClickListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    @BindView(R.id.tv_toolbar_title)
+    TextView tvToolbarTitle;
 
     @Override
     public int getLayoutId() {
@@ -39,19 +46,34 @@ public class MainActivity  extends BaseActivity implements View.OnClickListener{
      * @param menu
      */
     private void initSearch(Menu menu) {
-        MenuItem search = menu.findItem(R.id.mu_search);
+        //设置按钮的相关配置
         final MenuItem setting = menu.findItem(R.id.mu_setting);
+        View view = setting.getActionView();
+        //获取自定义布局的控件
+        tvStatus = (TextView) view.findViewById(R.id.tv_status);
+        //自定义布局添加点击事件，以此替换menu的id，如不需要改变menu显示值则取setting.getActionView()设置点击事件即可
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onOptionsItemSelected(setting);
+            }
+        });
+
+        //搜索的相关配置
+        MenuItem search = menu.findItem(R.id.mu_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
         searchView.setOnSearchClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setting.setVisible(false);
+                tvToolbarTitle.setVisibility(View.GONE);
             }
         });
         searchView.setOnCloseListener(new SearchView.OnCloseListener() {
             @Override
             public boolean onClose() {
                 setting.setVisible(true);
+                tvToolbarTitle.setVisibility(View.VISIBLE);
                 return false;
             }
         });
@@ -60,7 +82,7 @@ public class MainActivity  extends BaseActivity implements View.OnClickListener{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.mu_setting){
+        if (item.getItemId() == R.id.mu_setting) {
             //设置按钮
             showPopWindow();
         }
@@ -69,10 +91,11 @@ public class MainActivity  extends BaseActivity implements View.OnClickListener{
 
     private PopupWindow mPopWindow;
     private View mPopWindowView;
-    private TextView tvOne,tvTwo,tvThree;
-    private void showPopWindow(){
-        if(mPopWindowView == null){
-            mPopWindowView = LinearLayout.inflate(this,R.layout.menu_item_layout,null);
+    private TextView tvOne, tvTwo, tvThree, tvStatus;
+
+    private void showPopWindow() {
+        if (mPopWindowView == null) {
+            mPopWindowView = LinearLayout.inflate(this, R.layout.menu_item_layout, null);
             tvOne = (TextView) mPopWindowView.findViewById(R.id.tv_one);
             tvOne.setOnClickListener(this);
             tvTwo = (TextView) mPopWindowView.findViewById(R.id.tv_two);
@@ -80,25 +103,32 @@ public class MainActivity  extends BaseActivity implements View.OnClickListener{
             tvThree = (TextView) mPopWindowView.findViewById(R.id.tv_three);
             tvThree.setOnClickListener(this);
         }
-        if(mPopWindow == null){
-            mPopWindow = new PopupWindow(mPopWindowView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,true);
+        if (mPopWindow == null) {
+            mPopWindow = new PopupWindow(mPopWindowView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
             mPopWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             mPopWindow.setOutsideTouchable(true);
         }
-        mPopWindow.showAsDropDown(findViewById(R.id.mu_setting),-100,10);
+        mPopWindow.showAsDropDown(findViewById(R.id.mu_setting), -100, 10);
     }
 
     @Override
     public void onClick(View view) {
-        if(tvOne == view){
+        if (tvOne == view) {
             mPopWindow.dismiss();
-            showToast("One");
-        }else if(tvTwo == view){
+            tvStatus.setText("One");
+        } else if (tvTwo == view) {
             mPopWindow.dismiss();
-            showToast("Two");
-        }else if(tvThree == view){
+            tvStatus.setText("Two");
+        } else if (tvThree == view) {
             mPopWindow.dismiss();
-            showToast("Three");
+            tvStatus.setText("Three");
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
